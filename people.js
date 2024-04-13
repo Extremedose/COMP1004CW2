b2577f695ea88ace31e4514b019331ca04c05d32
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-13 14:50:28 +0100

    updated people html

--- people.js
+++ people.js
@@ -1,16 +1,38 @@
-function getSelectedOption(){
-    var option = document.getElementsByName("search_option");
+const peopleCSV = 'People.csv';
 
-    if (option[0].checked) {
-        console.log("Selected Option: " + option[0].value);
-        return option[0].value;
-    }
-    if (option[1].checked) {
-        console.log("Selected Option: " + option[1].value);
-        return option[1].value;
-    }
-    else{
-        console.log("No option was selected");
-    }
+function getSelectedOption() {
+  const option = document.getElementsByName("search_option");
+
+  if (option[0].checked) {
+    console.log("Selected Option: " + option[0].value);
+    filterByName();
+    return option[0].value;
+  }
+  if (option[1].checked) {
+    console.log("Selected Option: " + option[1].value);
+    filterByLicenceNo();
+    return option[1].value;
+  } else {
+    console.log("No option was selected");
+  }
 }
 
+function filterByName() {
+  const inputElement = document.getElementById("search_input_text");
+  const inputValueName = inputElement.value;
+  console.log("Name to search for:", inputValueName);
+  const column = 0;
+  readCSVFile(inputValueName, column);
+}
+
+function filterByLicenceNo() {
+  const inputElement = document.getElementById("search_input_text");
+  const inputValueLicenceNo = inputElement.value;
+  console.log("Licence number to search for:", inputValueLicenceNo);
+  const column = 4;
+  readCSVFile(inputValueLicenceNo, column);
+}
+
+function readCSVFile(dataToFilter, column) {
+  
+}
