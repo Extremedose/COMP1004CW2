4ff161b374e16fefa798c5b964d36ce18cd343a2
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-18 19:27:20 +0100

    update

--- vehicle.js
+++ vehicle.js
@@ -1,17 +1,20 @@
-import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from '@supabase/supabase-js';
 
-const SupabaseKey = 'https://xjmheuyoyvjlgfpfwdqs.supabase.co';
-const SupabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjg1MjAzOCwiZXhwIjoyMDI4NDI4MDM4fQ.jbCXYQ_eSysm4rGi5WDKu4e1UUnsmfSBrbC5VPHfV1Q';
+// Your Supabase URL and Key
+const SupabaseUrl = 'https://xjmheuyoyvjlgfpfwdqs.supabase.co';
+const SupabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjg1MjAzOCwiZXhwIjoyMDI4NDI4MDM4fQ.jbCXYQ_eSysm4rGi5WDKu4e1UUnsmfSBrbC5VPHfV1Q';
 
+// Supabase client options
 const options = {
-    db: {
-      schema: 'public',
-    },
-    auth: {
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: true
-    }
-  }
-  
-  const supabase = createClient(SupabaseUrl, SupabaseKey, options)
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+};
+
+// Create Supabase client instance
+const supabase = createClient(SupabaseUrl, SupabaseKey, options);
