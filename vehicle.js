1c7b8471dea3adcc03761ec65795e841e6dccb07
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-18 19:23:42 +0100

    update

--- vehicle.js
+++ vehicle.js
@@ -1,3 +1,5 @@
+import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
 const SupabaseKey = 'https://xjmheuyoyvjlgfpfwdqs.supabase.co';
 const SupabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjg1MjAzOCwiZXhwIjoyMDI4NDI4MDM4fQ.jbCXYQ_eSysm4rGi5WDKu4e1UUnsmfSBrbC5VPHfV1Q';
 
