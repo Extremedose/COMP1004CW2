3d3bb371ab8944ea12b069155775df39625fcc16
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-18 18:54:48 +0100

    removed

--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function () {
+
     const supabase = createClient('https://xjmheuyoyvjlgfpfwdqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTIwMzgsImV4cCI6MjAyODQyODAzOH0.8EcuwtTxa14bvPlVBlW_cseIKi40jaBhp90OKT9P9kc');
     try {
         const { data, error } = await supabase.from('People').select('*');
@@ -10,4 +10,3 @@ document.addEventListener('DOMContentLoaded', async function () {
     } catch (error) {
         console.error('Error fetching data:', error.message);
     }
-});
