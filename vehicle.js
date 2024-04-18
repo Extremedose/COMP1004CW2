e3053af436d108e97d10e2df87f8df3ba81141a4
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-18 19:06:52 +0100

    update

--- vehicle.js
+++ vehicle.js
@@ -1,12 +1,14 @@
-
-const supabase = createClient('https://xjmheuyoyvjlgfpfwdqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTIwMzgsImV4cCI6MjAyODQyODAzOH0.8EcuwtTxa14bvPlVBlW_cseIKi40jaBhp90OKT9P9kc');
-try {
-    const { data, error } = await supabase.from('People').select('*');
-    if (error) {
-        console.error(error);
-        return;
+document.addEventListener('DOMContentLoaded', async function () {
+    const supabase = createClient('https://xjmheuyoyvjlgfpfwdqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjg1MjAzOCwiZXhwIjoyMDI4NDI4MDM4fQ.jbCXYQ_eSysm4rGi5WDKu4e1UUnsmfSBrbC5VPHfV1Q');
+    
+    try {
+        const { data, error } = await supabase.from('People').select('*');
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
     }
-    console.log(data);
-} catch (error) {
-    console.error('Error fetching data:', error.message);
-}
+});
