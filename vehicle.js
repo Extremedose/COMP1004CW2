7348107ae7c31e12eb46b9688e89ed389f9cbbab
Psyaa25 <psyaa25@nottingham.ac.uk>
2024-04-19 16:03:14 +0100

    update

--- vehicle.js
+++ vehicle.js
@@ -1,20 +1,13 @@
-import { createClient } from './node_modules/@supabase/supabase-js'; // Use the correct relative path to the Supabase JS file
+const { createClient } = supabase
 
-// Your Supabase URL and Key
-const SupabaseUrl = 'https://xjmheuyoyvjlgfpfwdqs.supabase.co';
-const SupabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTIwMzgsImV4cCI6MjAyODQyODAzOH0.8EcuwtTxa14bvPlVBlW_cseIKi40jaBhp90OKT9P9kc';
+const _supabase = createClient('https://xjmheuyoyvjlgfpfwdqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqbWhldXlveXZqbGdmcGZ3ZHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTIwMzgsImV4cCI6MjAyODQyODAzOH0.8EcuwtTxa14bvPlVBlW_cseIKi40jaBhp90OKT9P9kc')
 
-// Supabase client options
-const options = {
-  db: {
-    schema: 'public',
-  },
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
-  },
-};
+console.log('Supabase Instance: ', _supabase)
 
-// Create Supabase client instance
-const supabase = createClient(SupabaseUrl, SupabaseKey, options);
+async function test(){
+
+  const { data, error } = await _supabase
+  .from("People")
+  .update(inputFields)
+  .eq("id", originalFields.id);
+}
